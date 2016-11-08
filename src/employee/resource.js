d009267b703b87385d87c20f4d9ca1d4a018219c
marchisbogdan <marchis_bogdan_nicolae@yahoo.com>
2016-11-08 19:59:27 +0200

    added the the save and search functions.

---
+++ src/employee/resource.js
@@ -0,0 +1,42 @@
+import {getLogger, ResourceError} from '../core/utils';
+import {apiUrl, authHeaders} from '../core/api';
+
+const log = getLogger(`employee/resource`);
+
+export const search = async (token) => {
+  const url = `${apiUrl}/employee`;
+  log(`GET ${url}`);
+  let ok;
+  let json = await fetch(url, {method: 'GET', headers: authHeaders(token)})
+    .then(res => {
+      ok = res.ok;
+      return res.json();
+    })
+
+  return interpretResult('GET', url,ok,json);
+}
+
+
+export const save = async(token,employee) => {
+  const body = JSON.stringify(employee);
+  const url = employee._id ? `${apiUrl}/note/${employee._id}` : `${apiUrl}/note`;
+  const method = employee._id ? 'PUT' : 'POST';
+  log(`${method} ${url}`);
+  let ok;
+  return fetch(url,{method, headers: authHeaders(token), body})
+    .then(res => {
+      ok = res.ok;
+      return res.json();
+    })
+  return interpretResult(method,url,ok,json);
+};
+
+function interpretResult(method,url,ok,json) {
+  if(ok){
+    log(`${method} ${url} succeeded`);
+    return json;  
+  }else{
+    log(`${method} ${url} failed`);
+    throw new ResourceError('Fetch failed', json.issue);
+  }
+}
